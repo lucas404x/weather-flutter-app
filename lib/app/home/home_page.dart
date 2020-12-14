0265b19634ca31aca6cd9fc1ddc927fb18f7958f
José Lucas da Silva <joselucas0303@gmail.com>
2020-12-14 13:12:08 -0300

    home prototype was created

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:weather_app/app/widgets/search_bar/search_bar.dart';
+import 'home_controller.dart';
+import '../widgets/search_bar/search_bar.dart';
 
 class HomePage extends StatelessWidget {
+  final _homeController = HomeController();
+
   @override
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
@@ -12,48 +15,60 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.black,
         body: Column(
-          children: [
-            Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                height: _size.width * .1,
-                width: _size.width * .1,
-                decoration:
-                    BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  height: _size.width * .1,
+                  width: _size.width * .1,
+                  decoration:
+                      BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Align(
+              SizedBox(
+                height: _size.height * .1,
+              ),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Hello Matt',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 32,
+                      color: Colors.white,
+                    ),
+                  )),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Check the weather by the city',
+                    style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                      color: Colors.white,
+                    ),
+                  )),
+              SizedBox(
+                height: _size.height * .06,
+              ),
+              SearchBar(
+                width: _size.width,
+                height: _size.height * .07,
+              ),
+              SizedBox(height: _size.height * .2),
+              Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  'Hello Matt',
+                  'My locations',
                   style: GoogleFonts.montserrat(
-                    fontSize: 32,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
                     color: Colors.white,
                   ),
-                )),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Check the weather by the city',
-                  style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 13,
-                    color: Colors.white,
-                  ),
-                )),
-            SizedBox(
-              height: 30,
-            ),
-            SearchBar(
-              width: _size.width,
-              height: _size.height * .07,
-            )
-          ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
