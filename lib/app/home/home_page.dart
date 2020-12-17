d4983bc5d481267d65d3e97740b062a16188e95e
José Lucas da Silva <joselucas0303@gmail.com>
2020-12-16 21:35:13 -0300

    added title, search bar component, and random background on home_screen

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,74 +1,97 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'home_controller.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import '../shared/const/image_api.dart';
+import '../shared/repositories/get_image.dart';
 import '../widgets/search_bar/search_bar.dart';
+import 'home_controller.dart';
 
 class HomePage extends StatelessWidget {
-  final _homeController = HomeController();
+  final _homeController = HomeController(GetImageRepository(
+      dio: Dio(BaseOptions(baseUrl: BASE_URL)),
+      accessKey: ACCESS_KEY,
+      randomImageEndpoint: GET_RANDOM_IMAGE_ENDPOINT));
 
   @override
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
 
-    return SafeArea(
-      minimum: const EdgeInsets.symmetric(vertical: 35.0, horizontal: 25.0),
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-            children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  height: _size.width * .1,
-                  width: _size.width * .1,
-                  decoration:
-                      BoxDecoration(color: Colors.white, shape: BoxShape.circle),
-                ),
-              ),
-              SizedBox(
-                height: _size.height * .1,
-              ),
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Hello Matt',
-                    style: GoogleFonts.montserrat(
-                      fontSize: 32,
-                      color: Colors.white,
-                    ),
+    return Stack(
+      children: [
+        FutureBuilder(
+            future: _homeController.getRandomImage(_size.height),
+            builder: (_, snapshot) => snapshot.hasData
+                ? FadeInImage.memoryNetwork(
+                    width: _size.width,
+                    height: _size.height,
+                    fit: BoxFit.fill,
+                    placeholder: kTransparentImage,
+                    image: snapshot.data)
+                : Container(
+                    color: Colors.black,
                   )),
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Check the weather by the city',
-                    style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 13,
-                      color: Colors.white,
-                    ),
-                  )),
-              SizedBox(
-                height: _size.height * .06,
-              ),
-              SearchBar(
-                width: _size.width,
-                height: _size.height * .07,
-              ),
-              SizedBox(height: _size.height * .2),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'My locations',
-                  style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 20,
-                    color: Colors.white,
+        SafeArea(
+          minimum: const EdgeInsets.symmetric(vertical: 35.0, horizontal: 25.0),
+          child: Scaffold(
+            body: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    height: _size.width * .1,
+                    width: _size.width * .1,
+                    decoration: BoxDecoration(
+                        color: Colors.white, shape: BoxShape.circle),
                   ),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: _size.height * .1,
+                ),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Hello Matt',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 32,
+                        color: Colors.white,
+                      ),
+                    )),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Check the weather by the city',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                        color: Colors.white,
+                      ),
+                    )),
+                SizedBox(
+                  height: _size.height * .06,
+                ),
+                SearchBar(
+                  width: _size.width,
+                  height: _size.height * .07,
+                ),
+                SizedBox(height: _size.height * .2),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'My locations',
+                    style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      );
+        )
+      ],
+    );
   }
 }
