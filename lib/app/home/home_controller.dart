fab8b84189f2fc82e68e8a967b5d42d90a29d502
José Lucas da Silva <joselucas0303@gmail.com>
2020-12-16 21:35:57 -0300

    created functions for get images

--- lib/app/home/home_controller.dart
+++ lib/app/home/home_controller.dart
@@ -1,3 +1,12 @@
+import '../shared/repositories/get_image_interface.dart';
+
 class HomeController {
-  const HomeController();
+  final IGetImage _getImage;
+  const HomeController(this._getImage);
+
+  Future<String> getRandomImage(double size) =>
+      _getImage.getRandomImage(size);
+
+  Future<String> getImageByCountry(String countryName) =>
+      _getImage.getImageByCountry(countryName);
 }
