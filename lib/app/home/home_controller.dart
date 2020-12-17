df9e50dff8f723037b7217148a6c2df77fcafe4e
José Lucas da Silva <joselucas0303@gmail.com>
2020-12-17 19:50:47 -0300

    Returning ImageModel now with more info for UI

--- lib/app/home/home_controller.dart
+++ lib/app/home/home_controller.dart
@@ -1,12 +1,20 @@
+import '../shared/const/image_api.dart';
+import '../shared/models/image.dart';
 import '../shared/repositories/get_image_interface.dart';
 
 class HomeController {
   final IGetImage _getImage;
   const HomeController(this._getImage);
 
-  Future<String> getRandomImage(double size) =>
-      _getImage.getRandomImage(size);
+  Future<ImageModel> getRandomImage() async {
+    Map<String, dynamic> parameters = {
+      'query': 'cities',
+      'client_id': ACCESS_KEY
+    };
 
-  Future<String> getImageByCountry(String countryName) =>
-      _getImage.getImageByCountry(countryName);
+    ImageModel image = await _getImage.getRandomImage(parameters);
+    return image;
+  }
+
+  Future<String> getImageByCountry(String countryName) => null;
 }
