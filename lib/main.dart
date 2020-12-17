d122586c66b13d26ba7c988007e0e51f6019deb6
José Lucas da Silva <joselucas0303@gmail.com>
2020-12-16 21:28:41 -0300

    ThemeData added on app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'app/home/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      scaffoldBackgroundColor: Colors.transparent
+    ),
     debugShowCheckedModeBanner: false,
     home: HomePage()));
 }
