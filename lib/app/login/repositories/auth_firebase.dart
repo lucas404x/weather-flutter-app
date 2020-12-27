99458949d29340ea2318c90d73587d9d498201cc
José Lucas da Silva <joselucas0303@gmail.com>
2020-12-27 19:42:20 -0300

    auth firebase signIn implemented

--- lib/app/login/repositories/auth_firebase.dart
+++ lib/app/login/repositories/auth_firebase.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:weather_app/app/shared/errors/auth_exception.dart';
 
+import '../../shared/models/firebase_user_model.dart';
 import '../../shared/models/user.dart';
 import 'auth_interface.dart';
 
@@ -9,8 +11,29 @@ class AuthFirebase implements IAuth {
   AuthFirebase(this._firebaseAuth);
 
   @override
-  Future<UserModel> signIn(Map<String, dynamic> credentials) {
-    // TODO: implement signIn
+  Future<UserModel> signIn(Map<String, dynamic> credentials) async {
+    UserCredential userCredential;
+
+    try {
+      userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+          email: credentials["email"], password: credentials["password"]);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'user-not-found':
+        case 'user-disable':
+          throw UserNotFoundException(e.code, e.message);
+        case 'invalid-email':
+        case 'wrong-password':
+          throw WrongCredentialsException(e.code, e.message);
+      }
+    }
+
+    return FirebaseUserModel.fromFirebaseUser(userCredential.user);
+  }
+
+  @override
+  Future<void> signUp(Map<String, dynamic> credentials) {
+    // TODO: implement signUp
     throw UnimplementedError();
   }
 }
--- lib/app/login/repositories/auth_interface.dart
+++ lib/app/login/repositories/auth_interface.dart
@@ -1,5 +1,6 @@
 import '../../shared/models/user.dart';
 
 abstract class IAuth {
+  Future<void> signUp(Map<String, dynamic> credentials);
   Future<UserModel> signIn(Map<String, dynamic> credentials);
 }
