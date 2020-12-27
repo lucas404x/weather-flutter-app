debed6971dc1c311f9c3d5eed188552a70707940
José Lucas da Silva <joselucas0303@gmail.com>
2020-12-27 19:42:37 -0300

    Custom exceptions implemented on app

--- lib/app/login/login_controller.dart
+++ lib/app/login/login_controller.dart
@@ -1,21 +1,45 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:weather_app/app/shared/errors/auth_exception.dart';
+
+import 'repositories/auth_interface.dart';
 
 class LoginController {
+  IAuth _auth;
   GlobalKey<FormState> _formKey;
-  GlobalKey<FormState> get formKey => _formKey;
+
+  TextEditingController _emailController;
+  TextEditingController _passwordController;
 
   final _stateButtonController = StreamController<bool>();
   Stream<bool> get isSigningIn => _stateButtonController.stream;
 
-  LoginController(this._formKey);
+  LoginController(this._auth, this._formKey, this._emailController,
+      this._passwordController);
 
-  Future<void> doAuth() async {
+  Future<void> doAuth(ScaffoldState scaffoldState) async {
     if (!_formKey.currentState.validate()) return;
-
+    // changing button to circular progress
     _stateButtonController.add(true);
-    // simulating an authentication
+
+    Map<String, dynamic> credentials = {
+      'email': _emailController.text,
+      'password': _passwordController.text
+    };
+
+    try {
+      await _auth.signIn(credentials);
+    } on AuthException catch (e) {
+      if (e.runtimeType == WrongCredentialsException) {
+        _stateButtonController.add(false);
+        var snackbar = SnackBar(content: Text(e.description));
+        scaffoldState.showSnackBar(snackbar);
+
+        return;
+      }
+    }
+
     await Future.delayed(Duration(seconds: 3));
     _stateButtonController.add(false);
   }
