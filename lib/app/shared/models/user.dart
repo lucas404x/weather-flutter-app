dea63ce85751a914bd5efc2a18b84f309cf6937f
José Lucas da Silva <joselucas0303@gmail.com>
2020-12-27 18:09:56 -0300

    user is an abstract class and firebase implements it

--- lib/app/shared/models/user.dart
+++ lib/app/shared/models/user.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/foundation.dart';
 
-class UserModel {
-  final String uuid;
+abstract class UserModel {
+  final String uid;
   final String email;
-  final String password;
 
-  UserModel(
-      {@required this.uuid, @required this.email, @required this.password});
-
-  factory UserModel.fromMap(Map<String, dynamic> data) => UserModel(
-      uuid: data["uuid"], email: data["email"], password: data["password"]);
-
-  Map<String, dynamic> toMap() =>
-      {'uuid': uuid, 'email': email, 'password': password};
+  UserModel({@required this.uid, @required this.email});
 }
