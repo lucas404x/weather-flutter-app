263e42a7afffbdbfbbb1828e378e40ebce2ec00d
José Lucas da Silva <joselucas0303@gmail.com>
2021-01-03 13:47:23 -0300

    removing the weather and temperature attributes of the model

--- lib/app/shared/models/location.dart
+++ lib/app/shared/models/location.dart
@@ -4,21 +4,21 @@ class LocationModel {
   final String name;
   final String locationKey;
   final String administrativeArea;
-  final String weather;
-  final double temperatureInCelsius;
 
-  LocationModel(
-      {@required this.name,
-      @required this.locationKey,
-      @required this.administrativeArea,
-      @required this.weather,
-      @required this.temperatureInCelsius});
+  LocationModel({
+    @required this.name,
+    @required this.locationKey,
+    @required this.administrativeArea,
+  });
+
+  factory LocationModel.fromMap(Map data) => LocationModel(
+      name: data['name'],
+      locationKey: data['locationKey'],
+      administrativeArea: data['administrativeArea']);
 
   Map<String, dynamic> toMap() => {
         'name': name,
         'locationKey': locationKey,
-        'administrativeArea': administrativeArea,
-        'weather': weather,
-        'temperatureInCelsius': temperatureInCelsius
+        'administrativeArea': administrativeArea
       };
 }
--- lib/app/shared/repositories/get_weather_info_interface.dart
+++ lib/app/shared/repositories/get_weather_info_interface.dart
@@ -1,6 +1,6 @@
 import '../../shared/models/location.dart';
 
 abstract class IGetWeatherInfo {
-  Future<LocationModel> getCurrentLocation(Map<String, dynamic> parameters);
+  Future<Map<String, dynamic>> getCurrentLocation(Map<String, dynamic> parameters);
   Future<List<LocationModel>> getMatchLocations(Map<String, dynamic> parameters);
 }
